583460b328dbfabfae75e38dbbe1d04454f35532
Sergey <Sergey@gmail.com>
2025-02-15 16:45:45 +0500

    Изменение main.py после проверок

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
-
-from src.widget import mask_account_card, get_date
-
-from src.widgett import mask_account_card, get_date
-
+from src.widget import get_date, mask_account_card
+from src.widgett import get_date, mask_account_card
 
 if __name__ == "__main__":
 
